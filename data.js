3d0493d9d600403a2722b044ca865fa059355637
cesargaleano <87844938+cesargaleano@users.noreply.github.com>
2023-03-04 01:09:43 -0500

    update data File

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const cv =
       "email": "tocega@gmail.com",
       "website": "https://example.com",
       "phone": "3013111225",
-      "avatar": "https://arepa.s3.amazonaws.com/melissa.jpg",
+      "avatar": "https://res.cloudinary.com/dvbadp1mn/image/upload/v1677908954/Profile/perfil_mod1_qahwpw.png",
       "Profile": "Full Stack Developer and professional in Electronic Engineering. With a proactive attitude and ability to propose alternative Web development solutions tailored to the needs of the current market, generating added value for clients. With the ability to work in a team ",
       "certificate": [
         {
@@ -16,72 +16,94 @@ const cv =
           "name": "Electronic Engineer"
         },
         {
-          "date": "Oct 2022",
-          "description": "Scratch at the door then walk away wack the mini furry mouse",
-          "institution": "Platzi",
-          "name": "Backend Developer"
+          "date": "Jun 2022",
+          "description": "Fullstack Developer Program",
+          "institution": "Soy Henry",
+          "name": "Fullstack Developer"
         }
       ],
       "Academic": [
         {
           "degree": "Electronic Engineer",
-          "description": " I’m so hungry i’m so hungry but ew not for that pelt around the house and up and down stairs chasing phantoms",
+          "description": "Electronic engineering is a branch of engineering that focuses on the design, development, manufacture, and maintenance of electronic devices and systems. This discipline focuses on the application of the principles of electronics and electricity to create systems and devices that are useful in a variety of industries and applications. ",
           "endDate": "Apr 2004",
           "institution": "Universidad de Santander UDES",
           "startDate": "Jul 1999"
         },
         {
-          "degree": "Space Engineering",
-          "description": "Meow in empty rooms. Find empty spot in cupboard and sleep all day check cat door for ambush 10 times before coming in for hack. ",
-          "endDate": "Decenber 2015",
-          "institution": "Harvard",
-          "startDate": "Jan 2017"
+          "degree": "Fullstack Developer",
+          "description": "The Fullstack Developer career is oriented towards programming tools such as JavaScript, CSS, Node.js and React along with other technologies that complete the professional development of the front and back-end. It was designed together with the main companies in the tech ecosystem, ensuring training in the languages and tools that the market requires. Thanks to an intense curriculum based on practice and real projects. ",
+          "endDate": "Jun 2022",
+          "institution": "Soy Henry",
+          "startDate": "Oct 2021"
         }
       ],
       "experience": [
         {
-          "company": "PugStar",
-          "endDate": "Present",
-          "jobDescription": "scratch the furniture. Good now the other hand, too lay on arms while you're using the keyboard.",
+          "company": "Azordev",
+          "endDate": "Jul 2022",
+          "jobDescription": "Freelance Developer for the development of the Trademarketing app with React Native Technology.",
           "jobTitle": "Frontend",
-          "startDate": "Jan 2020"
+          "startDate": "Sept 2022"
         },
         {
-          "company": "CatStore",
-          "endDate": "Jan 2016",
-          "jobDescription": "Meow in empty rooms lick left leg for ninety minutes, still dirty.",
-          "jobTitle": "Backend",
-          "startDate": "Sept 2019"
-        }
+          "company": "Soy Henry",
+          "endDate": "Jun 2022",
+          "jobDescription": "Participation as a Full Stack Developer in the Carry Collaborative Project, a Car Rental platform.",
+          "jobTitle": "FullStack",
+          "startDate": "Apr 2022"
+        },
+        {
+            "company": "No Country",
+            "endDate": "Jun 2022",
+            "jobDescription": "Participation as a Front End Developer in the PetLovers Collaborative Project, a Pet Shelter Management platform.",
+            "jobTitle": "FullStack",
+            "startDate": "Apr 2022"
+          }
+
       ],
       "skills": [
         {
           "name": "HTML5",
-          "percentage": "75%"
+          "percentage": "95%"
         },
         {
           "name": "CSS",
-          "percentage": "25%"
+          "percentage": "90%"
         },
         {
           "name": "JavaScript",
-          "percentage": "55%"
+          "percentage": "90%"
         },
         {
           "name": "React",
           "percentage": "90%"
+        },
+        {
+            "name": "Sequelize",
+            "percentage": "90%"
+        },
+        {
+            "name": "MongoDb",
+            "percentage": "90%"
+        },
+        {
+            "name": "Postgres",
+            "percentage": "90%"
         }
       ],
       "interest": [
         "javascript",
         "develop",
         "backend",
-        "frontend"
+        "frontend",
+        "fullstack",
+        "programming"
       ],
       "languages": [
         {
           "name": "Spanish",
-          "percentage": "90%"
+          "percentage": "98%"
         },
         {
           "name": "English",
